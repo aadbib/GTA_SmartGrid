aead1e45d6d88c1784c240b841eecfaa8534ef87
Achraf Adbib <achraf.adbib@hotmail.com>
2020-01-06 15:57:34 +0100

    Objecten worden aangemaakt

--- app/Main.py
+++ app/Main.py
@@ -1,9 +1,14 @@
 # Importeer libraries
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
+from models.Batterij import Batterij
+from models.Huis import Huis
+
 
 # Functie die de grid gaat tekenen
 def draw():
+    # https://stackoverflow.com/questions/24943991/change-grid-interval-and-specify-tick-labels-in-matplotlib
 
     # Creer een figure en daarin een plot
     fig = plt.figure()
@@ -26,9 +31,47 @@ def draw():
     # Toon de plot
     plt.show()
 
+def load_objects(wijk):
+
+    if wijk not in ['1', '2', '3']:
+        return "No such wijk!"
+
+    objects = {"huizen": [], "batterijen": []}
+
+    huizen_wijk = open(f'../data/wijk{wijk}_huizen.csv')
+    batterijen_wijk = open(f'../data/wijk{wijk}_batterijen.csv')
+
+    reader = csv.reader(huizen_wijk)
+
+    # Skip eerste regel
+    next(reader, None)
+
+    for x, y, output in reader:
+        huis = Huis((x, y), output)
+        objects["huizen"].append(huis)
+
+    huizen_wijk.close()
+
+    reader = csv.reader(batterijen_wijk)
+
+    # Skip eerste regel
+    next(reader, None)
+
+    for positie, capaciteit in reader:
+        batterij = Batterij(positie, capaciteit)
+        objects["batterijen"].append(batterij)
+
+    batterijen_wijk.close()
+
+    return objects
+
 # main functie
 def main():
+    wijk = str(input("Welke wijk wil je inladen?"))
+    objects = load_objects(wijk)
     draw()
+
+
 
 if __name__=="__main__":
     main()
